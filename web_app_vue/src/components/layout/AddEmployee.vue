c0e9dd6a7aaf800c4123454352a089e86fcda70e
Byron Jones <byronaj@uscupstate.edu>
2021-10-31 16:53:21 -0400

    Fixed skinny modal box

--- web_app_vue/src/components/layout/AddEmployee.vue
+++ web_app_vue/src/components/layout/AddEmployee.vue
@@ -1,89 +1,86 @@
 <template>
   <Modal v-on:close="toggleShowModal">
-    <div class="container">
+    <header class="modal-card-head">
+      <p class="modal-card-title">
+        Add Employee
+      </p>
+      <button class="delete" aria-label="close" @click="toggleShowModal"></button>
+    </header>
 
-      <header class="modal-card-head">
-        <p class="modal-card-title">
-          Add Employee
-        </p>
-        <button class="delete" aria-label="close" @click="toggleShowModal"></button>
-      </header>
+    <section class="modal-card-body">
+      <div class="field">
+        <label class="label">
+          First Name
+        </label>
+        <div class="control">
+          <input class="input is-info" type="text" v-model="employee.first_name" placeholder="First">
+        </div>
+      </div>
 
-      <section class="modal-card-body">
-        <div class="field">
-          <label class="label">
-            First Name
-          </label>
-          <div class="control">
-            <input class="input is-info" type="text" v-model="employee.first_name" placeholder="First">
+      <div class="field">
+        <label class="label">
+          Last Name
+        </label>
+        <div class="control">
+          <input class="input is-info" type="text" v-model="employee.last_name" placeholder="Last">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">
+          FTE Hours Allocated
+        </label>
+        <div class="control">
+          <div class="select is-info">
+            <select v-model="employee.fte">
+              <option value="1.0">1.0</option>
+              <option value="0.9">0.9</option>
+              <option value="0.8">0.8</option>
+              <option value="0.7">0.7</option>
+              <option value="0.6">0.6</option>
+              <option value="0.5">0.5</option>
+            </select>
           </div>
         </div>
+      </div>
 
-        <div class="field">
-          <label class="label">
-            Last Name
-          </label>
-          <div class="control">
-            <input class="input is-info" type="text" v-model="employee.last_name" placeholder="Last">
+      <div class="field">
+        <label class="label">
+          Shift
+        </label>
+        <div class="control">
+          <div class="select is-info">
+            <select v-model="employee.shift_block">
+              <option value="0">Variable</option>
+              <option value="1">First</option>
+              <option value="2">Second</option>
+              <option value="3">Third</option>
+            </select>
           </div>
         </div>
+      </div>
 
-        <div class="field">
-          <label class="label">
-            FTE Hours Allocated
+      <div class="field">
+        <label class="label">
+          Active
+        </label>
+        <div class="control">
+          <label class="checkbox">
+            <input type="checkbox"  v-model="employee.is_active">
           </label>
-          <div class="control">
-            <div class="select is-info">
-              <select v-model="employee.fte">
-                <option value="1.0">1.0</option>
-                <option value="0.9">0.9</option>
-                <option value="0.8">0.8</option>
-                <option value="0.7">0.7</option>
-                <option value="0.6">0.6</option>
-                <option value="0.5">0.5</option>
-              </select>
-            </div>
-          </div>
         </div>
+      </div>
+    </section>
 
-        <div class="field">
-          <label class="label">
-            Shift
-          </label>
-          <div class="control">
-            <div class="select is-info">
-              <select v-model="employee.shift_block">
-                <option value="0">Variable</option>
-                <option value="1">First</option>
-                <option value="2">Second</option>
-                <option value="3">Third</option>
-              </select>
-            </div>
-          </div>
-        </div>
+    <footer class="modal-card-foot">
+      <button class="button is-success" @click="submitForm">
+        Save changes
+      </button>
+      <button class="button" aria-label="close" @click="toggleShowModal">
+        Cancel
+      </button>
+    </footer>
 
-        <div class="field">
-          <label class="label">
-            Active
-          </label>
-          <div class="control">
-            <label class="checkbox">
-              <input type="checkbox"  v-model="employee.is_active">
-            </label>
-          </div>
-        </div>
-      </section>
-
-      <footer class="modal-card-foot">
-        <button class="button is-success" @click="submitForm">
-          Save changes
-        </button>
-        <button class="button" aria-label="close" @click="toggleShowModal">
-          Cancel
-        </button>
-      </footer>
-
-    </div>
   </Modal>
 </template>
 
