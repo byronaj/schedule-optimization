2bf17d48e1cca28ccb48fe7b557f3f423a446dda
Miro <miro@miroppb.com>
2021-11-11 12:19:33 -0500

    Added some constraints and changed how they're displayed

--- web_app_vue/src/components/layout/EditConstraints.vue
+++ web_app_vue/src/components/layout/EditConstraints.vue
@@ -2,7 +2,7 @@
   <Modal @close="toggleShowModal">
     <header class="modal-card-head">
         <p class="modal-card-title">
-        Edit Constraints
+        Edit Global Constraints
         </p>
         <button class="delete" aria-label="close" @click="toggleShowModal"></button>
     </header>
@@ -10,8 +10,36 @@
     <section class="modal-card-body" style="color:black;">
         <div class="field">
         <label class="checkbox">
-            <input type="checkbox" ref="shift12hours">
-            Shifts no more than 12 hours
+            <input type="number" style="width:45px;" max="12" min="1" value="8" ref="shifthours">
+            Maximum hour shifts
+        </label>
+        </div>
+        
+        <div class="field">
+        <label class="checkbox">
+            <input type="number" style="width:45px;" max="7" min="1" value="5" ref="maxwork">
+            Maximum consecutive working days
+        </label>
+        </div>
+        
+        <div class="field">
+        <label class="checkbox">
+            <input type="number" style="width:45px;" max="7" min="1" value="2" ref="minwork">
+            Minimum consecutive working days
+        </label>
+        </div>
+        
+        <div class="field">
+        <label class="checkbox">
+            <input type="number" style="width:45px;" max="7" min="1" value="2" ref="minfree">
+            Minimum consecutive free days
+        </label>
+        </div>
+        
+        <div class="field">
+        <label class="checkbox">
+            <input type="number" style="width:45px;" max="7" min="1" value="2" ref="maxweekend">
+            Maximum consecutive weekends
         </label>
         </div>
 
@@ -22,7 +50,6 @@
         </label>
         </div>
     </section>
-
     <footer class="modal-card-foot">
         <button class="button is-success" @click="saveConstraints">
         Save changes
@@ -41,14 +68,26 @@ import Modal from "@/components/Modal.vue";
 export default {
   components: { Modal },
   name: 'EditConstraints',
+  data() {
+		return {
+			constraints: []
+		};
+	},
   methods: {
       toggleShowModal() {
           this.$parent.toggleEditConstraints()
       },
       saveConstraints() {
-        let shift12hours = this.$refs.shift12hours.checked
-        let back2back = this.$refs.back2back.checked
+        this.constraints.push(["Max hour shifts", this.$refs.shifthours.value])
+        this.constraints.push(["Max consecutive working days", this.$refs.maxwork.value])
+        this.constraints.push(["Min consecutive working days", this.$refs.minwork.value])
+        this.constraints.push(["Min consecutive free days", this.$refs.minfree.value])
+        this.constraints.push(["Max consecutive weekends", this.$refs.maxweekend.value])
+        this.constraints.push(["Back-to-Back Shifts", this.$refs.back2back.checked])
         //save constraints
+      },
+      getConstraints() {
+          return this.constraints
       }
   }
 }
