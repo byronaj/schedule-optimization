6645b89e7358e2e915433c113b2e99d1bea09991
takyt <tkyte@email.uscupstate.edu>
2021-12-02 14:57:46 -0500

    Fixed tests to work with new output formatting of the solver function

--- web_app_django/tests/test_solver.py
+++ web_app_django/tests/test_solver.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 from web_app.solver import solve_shift_scheduling
 
 workers = 8  # workers
@@ -6,6 +7,7 @@ num_shifts = 4
 weeks = 1  # weeks
 stats = [[0] * (num_shifts * 2) for _ in range(workers)]
 sCount = [[0] * num_shifts for _ in range(weeks * 7)]
+result = [[0] * weeks * 7 for _ in range(workers)]
 
 
 def set_stats(result):
@@ -25,7 +27,7 @@ def set_stats(result):
             else:
                 previous_shift = 'V'
 
-            if shift == 'O':
+            if shift == '0':
                 stats[i][0] = stats[i][0] + 1
                 sCount[j][0] = sCount[j][0] + 1
                 if shift == previous_shift:
@@ -34,7 +36,7 @@ def set_stats(result):
                         stats[i][1] = streak
                 else:
                     streak = 1
-            elif shift == 'M':
+            elif shift == '1':
                 stats[i][2] = stats[i][2] + 1
                 sCount[j][1] = sCount[j][1] + 1
                 if shift == previous_shift:
@@ -43,7 +45,7 @@ def set_stats(result):
                         stats[i][3] = streak
                 else:
                     streak = 1
-            elif shift == 'A':
+            elif shift == '2':
                 stats[i][4] = stats[i][4] + 1
                 sCount[j][2] = sCount[j][2] + 1
                 if shift == previous_shift:
@@ -52,7 +54,7 @@ def set_stats(result):
                         stats[i][5] = streak
                 else:
                     streak = 1
-            elif shift == 'N':
+            elif shift == '3':
                 stats[i][6] = stats[i][6] + 1
                 sCount[j][3] = sCount[j][3] + 1
                 if shift == previous_shift:
@@ -73,7 +75,8 @@ class test_solver(unittest.TestCase):
     def setUpClass(cls):
         # if any constraint is changed make sure to change the associated test parameters
         # or tests will not give valid feedback
-        shifts = ['O', 'M', 'A', 'N']  # shifts
+        emp_ids = [0, 1, 2, 3, 4, 5, 6, 7, 8]  # need same number of id's as employees
+        shifts = [0, 1, 2, 3]  # shifts
         shift_constraints = [(0, 1, 1, 0, 2, 2, 0),  # off at least one day but no more than 2 in a row
                              (3, 1, 2, 20, 3, 4,
                               5)]  # third shift at one day preferably 2 days in a row but no more than 4
@@ -90,7 +93,8 @@ class test_solver(unittest.TestCase):
             (1, 2, 3),  # Saturday
             (1, 3, 1),  # Sunday
         ]
-        result = solve_shift_scheduling(
+        resultu = solve_shift_scheduling(
+            emp_ids,
             workers,
             weeks,
             shifts,
@@ -99,7 +103,18 @@ class test_solver(unittest.TestCase):
             penalized_transitions,
             weekly_cover_demands
         )
-        print('result in test case: ')
+        for i in range(len(resultu)):
+            for key in (resultu[i]):
+                if key == 'shift_assignments':
+                    print(str(resultu[i][key]))
+                    # this gets a list of all info for each employee
+                    info = str(resultu[i][key])
+                    # this is regex that finds the number after the word assignment to get shift info
+                    m = re.findall("'assignment': (\d+)", info)
+                    num = m
+                    for x in range(len(num)):
+                        result[i][x] = num[x]
+
         print(result)
         set_stats(result)
 
